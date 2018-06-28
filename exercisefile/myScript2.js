cdb08bb66c6d214fbe42dd3ee6d8227548d2e0de
WeirdlyWired <38538519+shomikg@users.noreply.github.com>
2018-06-28 15:07:34 +0530

    Exercise and Switch statement

---
+++ exercisefile/myScript2.js
@@ -0,0 +1,19 @@
+var num1= 15;
+var num2= 7;
+var num3= 3;
+
+if(num1>num2)
+{
+if(num1>num3)
+document.write(num1);
+else
+document.write(num3);
+}
+
+if(num1<num2)
+{
+  if(num2>num3)
+  document.write(num2);
+  else
+    document.write(num3);
+}
