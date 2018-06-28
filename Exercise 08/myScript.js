2276299ab0221cac3d0c68928290cec14462e79b
WeirdlyWired <38538519+shomikg@users.noreply.github.com>
2018-06-28 12:34:50 +0530

    Working with Concatenating strings

---
+++ Exercise 08/myScript.js
@@ -0,0 +1,24 @@
+/*     Concatenation "+" Operator     */
+
+// concatenate two strings
+
+var firstName = "Irfan";
+var lastName = "Dayan";
+
+var fullName = firstName + " " + lastName;
+
+document.write(fullName);
+
+
+document.write("<br>");
+
+
+// add number & string
+
+var x = 5;
+//var t = "5";
+var t = "Hello";
+var z = t + x;
+
+document.write(z);
+document.write("<br>");
