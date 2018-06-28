4dc095c8a51e3cc9f43190ca36ff552b696468a5
WeirdlyWired <38538519+shomikg@users.noreply.github.com>
2018-06-28 11:54:26 +0530

    Added operations

--- Exercise 06/myScript.js
+++ Exercise 06/myScript.js
@@ -9,3 +9,43 @@
 --    Decrement
 
 */
+var x = 100, y = 20;
+var a = x + y;
+document.write("Addition : ");
+document.write(a);
+
+document.write("<br>");
+
+var s = x - y;
+document.write("Subtraction : ");
+document.write(s);
+
+document.write("<br>")
+
+var m = x * y;
+document.write("Multiplication : ");
+document.write(m);
+
+document.write("<br>")
+
+var n = x / y;
+document.write("Division : ");
+document.write(n);
+
+document.write("<br>")
+
+var u = x % y;
+document.write("Modulus : ");
+document.write(u);
+
+document.write("<br>")
+
+var j = x++;
+document.write("Increment : ");
+document.write(j);
+
+document.write("<br>")
+
+var k = x--;
+document.write("Decrement : ");
+document.write(k);
