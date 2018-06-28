079f5f0546aee9ff98765797413543176c4586f5
WeirdlyWired <38538519+shomikg@users.noreply.github.com>
2018-06-28 11:29:12 +0530

    Working with Variables - I

---
+++ Exercise 04/myScript.js
@@ -0,0 +1,28 @@
+//Javascript Variables
+
+//value = undefined by default
+var myFirstVariable; //undefined
+document.write(myFirstVariable);
+
+var myName; //variable declaration
+document.write("<br>");// html line break
+//=assignment operator
+myName = "Irfan"; //varible initialization
+
+document.write(myName);
+
+document.write("<br>");// html line break
+
+var yourName = "Daniel"; // declaration & initialization at the same time
+
+document.write(yourName);
+
+myCar = "BMW"; //declaration & initialization without the var keyword
+
+var x,y,z
+
+var x = 10, y=20, z = 50; //multi variables declaration & initialization
+
+var serialNumber = 87999;
+
+document.write(serialNumber);
