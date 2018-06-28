4f2581e0059f741d0977166bad3b181914bc7baf
WeirdlyWired <38538519+shomikg@users.noreply.github.com>
2018-06-28 16:37:24 +0530

    js

--- Exercise 16/myScript.js
+++ Exercise 16/myScript.js
@@ -2,11 +2,11 @@
 
 // function with parameters
 function functionWithParameters( p1, p2 ) {
-	
+
 	var z = p1 * p2;
-	
+
 	document.write("p1 * p2 = " + z + "<br>");
-	
+
 }
 
 // function call with arguments
@@ -15,15 +15,15 @@ functionWithParameters( 10, 20 );
 
 // accpet function as a value
 function parameterAsFunction ( func ) {
-	
+
 	func();
-	
+
 }
 
 parameterAsFunction( hellWorld );
 
 function helloWorld() {
-	
+
 	alert("Hello, World. Function passed as an argument.");
 }
 
@@ -31,22 +31,15 @@ function helloWorld() {
 
 // function with return statement
 function returnSomeValue ( x, y ) {
-	var k = x + y	
+	var k = x + y
 	return  k;
 }
 
 /*var result = returnSomeValue( 10, 10 );
 
  document.write("Returned value is " + result);
-*/ 
+*/
 
 // functions as values
 
 document.write("Returned value is " + returnSomeValue( 10, 10 ) );
-
-
-
-
-
-
-
