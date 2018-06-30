d0f7a5446e8252ce75357247cfad333299f5c9a6
WeirdlyWired <38538519+shomikg@users.noreply.github.com>
2018-07-01 01:12:01 +0530

    Exercise 7

---
+++ exercisefile/myScript7.js
@@ -0,0 +1,11 @@
+var pElement = document.getElementById("my-p");
+
+// using style property
+/*pElement.style.color = "green";
+pElement.style.fontSize = "35px";
+pElement.style.border = "1px solid black";
+pElement.style.padding = "18px";
+
+*/
+// you can also use className property
+pElement.className = "my-p";
