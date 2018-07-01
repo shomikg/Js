85f7e352d29cfbf2370cff75d57b867c6cc65282
WeirdlyWired <38538519+shomikg@users.noreply.github.com>
2018-07-01 17:57:03 +0530

    ECMA 5 Strict Mode

---
+++ Exercise 51/myScript.js
@@ -0,0 +1,20 @@
+// JavaScript - Strict Mode
+
+//"use strict";
+
+x = 10;
+alert(x);
+
+// Non-strict code...
+
+(function(){
+  "use strict";
+
+  // Define your library strictly...
+    y = 20;
+    alert(y);
+    
+    
+})();
+
+// Non-strict code..
